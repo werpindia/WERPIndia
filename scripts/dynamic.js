bb29c2f95d41d429748fd7535e79cebfd3aee9c9
shashwatsr2712 <shashwatsr2712@gmail.com>
2019-06-20 15:10:45 +0530

    Add files via upload

---
+++ scripts/dynamic.js
@@ -0,0 +1,9 @@
+//Enable Swipe Left/Right action for carousel on mobile devices
+/*$(document).ready(function(){  
+    $(".carousel-inner").swiperight(function(){  
+        $(this).parent().carousel('prev');  
+    });  
+    $(".carousel-inner").swipeleft(function(){  
+        $(this).parent().carousel('next');  
+    });  
+});*/
