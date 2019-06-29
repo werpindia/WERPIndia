95fc32477c3ed3845b8ed89ec32d980481d4ca4b
shashwatsr2712 <shashwatsr2712@gmail.com>
2019-06-29 16:52:51 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,19 @@
+<footer id="foot">
+    <div class="row">
+        <div class="col-md-6">
+            <h2 style="font-family: aclonica;">CONTACT</h2>
+            <hr/>
+            <p>Email: hr@werpindia.org</p>
+            <address>T-318/10, First floor, Balijeet Nagar, New Delhi-08</address><br/>
+            <a href="https://www.facebook.com/WERPIndia.org12" target="_blank" class="awesome fab fa-facebook" style="color:white;text-decoration:none;"></a>&nbsp;
+            <a href="https://www.youtube.com/watch?v=_vabo-oP44c&t=12s" target="_blank" class="awesome fab fa-youtube" style="color:white;text-decoration:none;"></a>&nbsp;
+            <a href="mailto:hr@werpindia.org" target="_blank" class="awesome fas fa-envelope-square" style="color:white;text-decoration:none;"></a>&nbsp;
+            <hr/>
+        </div>
+        <div class="col-md-6">
+            <iframe width="300" height="300" src="https://maps.google.com/maps?q=t-318%20block%20b%20baljit%20nagar%20delhi%20110008&amp;t=k&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+            </iframe>
+            <a href="https://www.pureblack.de/webdesign-darmstadt/"></a>
+        </div>
+    </div>    
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,62 @@
+<!--Navbar-->
+<nav class="navBar">
+    <div class="navBrand">
+        <a href="index.php"><img class="img-fluid" src="https://i.ibb.co/ZNDm012/logo.jpg"/></a>
+    </div>
+    <div class="hamburger">
+        <div class="bar1"></div>
+        <div class="bar2"></div>
+        <div class="bar3"></div>
+    </div>
+    <div class="navList">
+        <?php if(isset($_SESSION['role'])){ ?> 
+        <ul>
+            <li><a href="blogs.php">BLOGS</a></li>
+            <li><a href="javascript:openSideNav()">ACCOUNT</a></li>
+            <li><a href="logout.php">LOGOUT</a></li>   
+        </ul>
+        <?php } else if(isset($_SESSION['id'])){ ?> 
+        <ul>
+            <li><a href="blogs.php">BLOGS</a></li>
+            <li><a href="javascript:openSideNav()">ACCOUNT</a></li>
+            <li><a href="logout.php">LOGOUT</a></li>   
+        </ul>
+        <?php } else{ ?> 
+        <ul>
+            <li><a href="blogs.php">BLOGS</a></li>
+            <li><a href="login.php">LOGIN</a></li>   
+        </ul>
+        <?php } ?>
+    </div>
+</nav>
+<!--Navigation menu for mobile devices-->
+<div class="navListMobile">
+    <?php if(isset($_SESSION['role'])){ ?> 
+    <ul>
+        <li><a href="blogs.php">BLOGS</a></li>
+        <li><a href="javascript:openSideNav()">ACCOUNT</a></li>
+        <li><a href="logout.php">LOGOUT</a></li>   
+    </ul>
+    <?php } else if(isset($_SESSION['id'])){ ?> 
+    <ul>
+        <li><a href="blogs.php">BLOGS</a></li>
+        <li><a href="javascript:openSideNav()">ACCOUNT</a></li>
+        <li><a href="logout.php">LOGOUT</a></li>   
+    </ul>
+    <?php } else{ ?> 
+    <ul>
+        <li><a href="blogs.php">BLOGS</a></li>
+        <li><a href="login.php">LOGIN</a></li>   
+    </ul>
+    <?php } ?>       
+</div>
+
+<div id="mySideNav" class="sideNav">
+    <a href="javascript:void(0)" class="closebtn" onclick="closeSideNav()">&times;</a>
+    <a href="blogs.php?userId=<?php echo $_SESSION['id'];?>">My Blogs</a>
+    <a href="new.php">Create New Blog</a>
+    <?php if(isset($_SESSION['role'])){?>
+    <a href="pending.php">Pending Verifications</a>
+    <?php }?>
+    <a href="settings.php">Change Password</a>
+</div>
