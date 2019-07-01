16646b5ecaa31409b34558125a9ea3a524e14160
shashwatsr2712 <shashwatsr2712@gmail.com>
2019-07-01 14:27:24 +0530

    Update new.php

--- new.php
+++ new.php
@@ -10,10 +10,32 @@
         <!--Custom External stylesheet-->
         <link rel="stylesheet" type="text/css" href="styles/header.css"/>
         <link rel="stylesheet" type="text/css" href="styles/footer.css"/>
+        <!--TinyMCE Editor CDN-->
+        <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js"></script>
+        <!--TinyMCE Editor Script-->
+        <script>
+            $(document).ready(function(){
+                tinymce.init({
+                    selector:'textarea#content',
+                    height:"400px",
+                    setup: function(editor){
+                        editor.on('change', function(){
+                            editor.save();
+                        });
+                    }
+                });
+            });
+        </script>
         <style>
+            html,body{
+                overflow-x:visible !important;
+            }
             label{
                 font-weight:bold;
                 font-family:futura,rockwell,"Helvetica Neue","Super Grotesk",Montserrat;
+            }
+            .tox-notifications-container{
+                display:none;
             }
         </style>
     </head>
@@ -48,14 +70,7 @@
             </div>
             <div class="form-group">
                 <label for="content">Blog Content</label>
-                <div class="alert alert-info alert-dismissible fade show">
-                    <button type="button" class="close" data-dismiss="alert">&times;</button>
-                    <strong>For paragraphs, wrap the content with &lt;p&gt;&lt;/p&gt;. e.g.:&lt;p&gt;This is a paragraph...&lt;/p&gt;</strong><br/>
-                    <strong>For bold text, use &lt;strong&gt;Text to be bolded...&lt;/strong&gt;</strong><br/>
-                    <strong>To italicize text, use &lt;em&gt;Text to be italicized...&lt;/em&gt;</strong><br/>
-                    <strong>To give a line break, use &lt;br&gt;. e.g.:&lt;br&gt;This will move to next line...</strong><br/>
-                </div>
-                <textarea class="form-control" required="required" rows="10" name="description" id="content"></textarea>
+                <textarea class="form-control" required="required" name="description" id="content"></textarea>
             </div>
             <button class="btn btn-outline-primary" type="submit">Submit</button>
         </form>
