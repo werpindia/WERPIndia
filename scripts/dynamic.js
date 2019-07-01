456df321bb0f0f425cab1e71866b7f2d1a37da1d
shashwatsr2712 <shashwatsr2712@gmail.com>
2019-07-01 14:29:07 +0530

    Update dynamic.js

--- scripts/dynamic.js
+++ scripts/dynamic.js
@@ -39,34 +39,24 @@ $(document).ready(function(){
     $('.marquee').marquee({
         //If you wish to always animate using jQuery
         allowCss3Support: true,
-        
         //works when allowCss3Support is set to true
         css3easing: 'linear',
-        
         //requires jQuery easing plugin. Default is 'linear'
         easing: 'linear',
-        
         //pause time before the next animation turn in milliseconds
         delayBeforeStart: 1000,
-        
         //'left', 'right', 'up' or 'down'
         direction: 'left',
-        
         //true or false - should the marquee be duplicated to show an effect of continues flow
         duplicated: false,
-        
         //speed in milliseconds of the marquee
         duration: 5500,
-        
         //gap in pixels between the tickers
         gap: 20,
-        
         //on cycle pause the marquee
         pauseOnCycle: false,
-
         //on hover pause the marquee - using jQuery plugin https://github.com/tobia/Pause
         pauseOnHover: true,
-        
         //the marquee is visible initially positioned next to the border towards it will be moving
         startVisible: false
     });
