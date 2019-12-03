11cd3bf3dcd11dbb4377046861b18e13436f8c78
shashwatsr2712 <shashwatsr2712@gmail.com>
2019-12-03 12:05:59 +0530

    Add files via upload

--- includes/footer.php
+++ includes/footer.php
@@ -3,11 +3,11 @@
         <div class="col-md-6">
             <h2 style="font-family: aclonica;">CONTACT</h2>
             <hr/>
-            <p>Email: hr@werpindia.org</p>
+            <p>Email: WerpHumanResource@gmail.com</p>
             <address>T-318/10, First floor, Balijeet Nagar, New Delhi-08</address><br/>
             <a href="https://www.facebook.com/WERPIndia.org12" target="_blank" class="awesome fab fa-facebook" style="color:white;text-decoration:none;"></a>&nbsp;
             <a href="https://www.youtube.com/watch?v=_vabo-oP44c&t=12s" target="_blank" class="awesome fab fa-youtube" style="color:white;text-decoration:none;"></a>&nbsp;
-            <a href="mailto:hr@werpindia.org" target="_blank" class="awesome fas fa-envelope-square" style="color:white;text-decoration:none;"></a>&nbsp;
+            <a href="mailto:WerpHumanResource@gmail.com" target="_blank" class="awesome fas fa-envelope-square" style="color:white;text-decoration:none;"></a>&nbsp;
             <hr/>
         </div>
         <div class="col-md-6">
@@ -15,5 +15,12 @@
             </iframe>
             <a href="https://www.pureblack.de/webdesign-darmstadt/"></a>
         </div>
-    </div>    
+    </div>
+    <div class="row">
+        <div class="offset-1 col-10">
+            <p style="font-style:italic;">This Website was designed and developed by WERP-India Web-Development Group under the WomenUp initiatives. We build websites free for Women startup owner and Youth entrepreneurs. And let help you to start up your idea from A-Z. <a href="https://www.linkedin.com/pulse/what-werp-seed-capital-gabriel-shitu">Read More...</a></p> 
+            <hr/>
+            <p style="font-weight:bold;color:#3A8DDc;">&copy 2017-2019 Copyright | WERP-India</p>
+        </div>
+    </div>
 </footer>
--- includes/header.php
+++ includes/header.php
@@ -11,24 +11,26 @@
     <div class="navList">
         <?php if(isset($_SESSION['role'])){ ?> 
         <ul>
-            <li><a href="blogs.php">BLOGS</a></li>
+            <li><a href="blogs.php">VOICE4WOMEN</a></li>
             <li><a href="javascript:openSideNav()">ACCOUNT</a></li>
             <li><a href="logout.php">LOGOUT</a></li>   
         </ul>
         <?php } else if(isset($_SESSION['id'])){ ?> 
         <ul>
-            <li><a href="blogs.php">BLOGS</a></li>
+            <li><a href="blogs.php">VOICE4WOMEN</a></li>
             <li><a href="javascript:openSideNav()">ACCOUNT</a></li>
             <li><a href="logout.php">LOGOUT</a></li>   
         </ul>
         <?php } else{ ?> 
         <ul>
-            <li><a href="blogs.php">BLOGS</a></li>
+            <li><a href="blogs.php">VOICE4WOMEN</a></li>
             <li><a href="login.php">LOGIN</a></li>   
         </ul>
         <?php } ?>
     </div>
 </nav>
+
+
 <!--Navigation menu for mobile devices-->
 <div class="navListMobile">
     <?php if(isset($_SESSION['role'])){ ?> 
@@ -53,10 +55,14 @@
 
 <div id="mySideNav" class="sideNav">
     <a href="javascript:void(0)" class="closebtn" onclick="closeSideNav()">&times;</a>
+    <?php if(isset($_SESSION['role'])){ ?>
     <a href="blogs.php?userId=<?php echo $_SESSION['id'];?>">My Blogs</a>
     <a href="new.php">Create New Blog</a>
-    <?php if(isset($_SESSION['role'])){?>
     <a href="pending.php">Pending Verifications</a>
-    <?php }?>
     <a href="settings.php">Change Password</a>
+    <?php } else{ ?>
+    <a href="blogs.php?userId=<?php echo $_SESSION['id'];?>">My Blogs</a>
+    <a href="new.php">Create New Blog</a>
+    <a href="settings.php">Change Password</a>
+    <?php } ?>
 </div>
